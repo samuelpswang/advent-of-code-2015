680699badc036330f48291d2a2e541fe403bef1f
samuelpswang <samuel.wang21@imperial.ac.uk>
2023-12-08 16:44:07 +0000

    feat: day 04 challenges with external md5 code

---
+++ day_04/part_2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+// implement your own md5 -
+#include "md5.hpp"
+
+bool is_valid(const string& key, long num) {
+    string hash = md5(key+to_string(num));
+    for (int i = 0; i < 6; i++) {
+        if (hash.at(i) != '0') return false;
+    }
+    return true;
+}
+
+long linear_search(const string& key) {
+    long first = 0;
+    while (!is_valid(key, first)) { ++first; }
+    return first;
+}
+
+int main() {
+    ifstream ifs("input.txt");
+    string key;
+    getline(ifs, key);
+    ifs.close();
+    cout << linear_search(key) << endl;
+}
