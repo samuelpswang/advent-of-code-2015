5853aad26110a7a929c4c5c96915c3486c5c8743
samuelpswang <samuel.wang21@imperial.ac.uk>
2023-12-31 23:35:13 +0000

    feat: update day 04

---
+++ day_04/part_2.py
@@ -0,0 +1,15 @@
+from hashlib import md5
+
+def is_valid(key, num):
+    hexhash = md5((key+str(num)).encode()).hexdigest()
+    return all(hexhash[i] == '0' for i in range(6))
+
+key = ""
+with open("input.txt", "r") as file:
+    key = file.readline()
+
+num = 0
+while not is_valid(key, num): 
+    num += 1
+
+print(num)
