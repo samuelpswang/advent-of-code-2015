b822939916c699c30daf454cd2bfbddf58a19b3c
samuelpswang <samuel.wang21@imperial.ac.uk>
2024-01-04 23:35:22 +0200

    feat: day 15 challenge

---
+++ day_15/part_2.py
@@ -0,0 +1,40 @@
+from gekko import GEKKO
+
+# read ingredients
+ingredients = []
+with open("input.txt", "r") as file:
+    for line in file:
+        _, line = line.strip("\n").split(": ")
+        line = line.split(", ")
+        line = [int(item.split()[1]) for item in line]
+        ingredients.append(line)
+
+# initialize gekko
+m = GEKKO()
+m.options.SOLVER = 1
+
+# Initialize variables
+x1 = m.Var(value=100, lb=0, ub=100, integer=True)
+x2 = m.Var(value=0, lb=0, ub=100, integer=True)
+x3 = m.Var(value=0, lb=0, ub=100, integer=True)
+x4 = m.Var(value=0, lb=0, ub=100, integer=True)
+
+# minimize objective function
+m.Obj((x1 * ingredients[0][0] + x2 * ingredients[1][0] + x3 * ingredients[2][0] + x4 * ingredients[3][0]) * \
+      (x1 * ingredients[0][1] + x2 * ingredients[1][1] + x3 * ingredients[2][1] + x4 * ingredients[3][1]) * \
+      (x1 * ingredients[0][2] + x2 * ingredients[1][2] + x3 * ingredients[2][2] + x4 * ingredients[3][2]) * \
+      (x1 * ingredients[0][3] + x2 * ingredients[1][3] + x3 * ingredients[2][3] + x4 * ingredients[3][3]) * (-1))
+
+# constraints
+m.Equation((x1 + x2 + x3 + x4) == 100)
+m.Equation((x1 * ingredients[0][0] + x2 * ingredients[1][0] + x3 * ingredients[2][0] + x4 * ingredients[3][0]) >= 0)
+m.Equation((x1 * ingredients[0][1] + x2 * ingredients[1][1] + x3 * ingredients[2][1] + x4 * ingredients[3][1]) >= 0)
+m.Equation((x1 * ingredients[0][2] + x2 * ingredients[1][2] + x3 * ingredients[2][2] + x4 * ingredients[3][2]) >= 0)
+m.Equation((x1 * ingredients[0][3] + x2 * ingredients[1][3] + x3 * ingredients[2][3] + x4 * ingredients[3][3]) >= 0)
+m.Equation((x1 * ingredients[0][4] + x2 * ingredients[1][4] + x3 * ingredients[2][4] + x4 * ingredients[3][4]) == 500)
+
+# solve
+m.solve(disp=False)
+
+# print results
+print(int(m.options.objfcnval) * (-1))
